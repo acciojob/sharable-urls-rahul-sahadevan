22a4f5ec90e1dead744d19a22cc755302feed610
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-24 10:38:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+const h3 = document.getElementById("url");
+const button = document.getElementById("button");
+button.addEventListener("click",() =>{
+    const name = document.getElementById("name").value;
+    // console.log(name);
+	const year = document.getElementById("year").value;
+    if(year === ""){
+        h3.innerText = h3.innerText+"?name="+name;
+    }
+    else if(name === ""){
+        h3.innerText = h3.innerText+"?year="+year;
+    }
+    else{
+
+        h3.innerText= h3.innerText+"?name="+name+"&year="+year;
+    }
+})
